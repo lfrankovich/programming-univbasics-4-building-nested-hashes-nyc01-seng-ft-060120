20d2002d812d075fcfb6f990a3d8147fdab7fd84
Lauren Frankovich <frankovister@gmail.com>
2020-05-25 16:30:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {:railroads => {
-    :pieces
-  }}
+    :pieces => 4
+  }
+  }
 end
 
 def monopoly_with_third_tier
